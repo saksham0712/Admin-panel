54ff05c24272635baa4726529b03667fdd665c05
Saksham Verma <sakshamverma626@gmail.com>
2024-07-02 21:56:53 +0530

    Home Completedd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import { Home } from './pages/Home'
+import Home from './pages/Home'
+import './App.css'
 
 function App() {
  
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../Styles/HomeStyle.css'
-import { Navbar } from '../components/Navbar'
-export const Home = () => {
-  return (
-    <>
-    <Navbar />
-    </>
-  )
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <>
+            <div className='home-main d-flex justify-content-center align-items-center vw-100 vh-100'>
+                <div className='home-main-text d-flex flex-column justify-content-center align-items-center'>
+                    <h2>
+                        Welcome
+                    </h2>
+                    <p>Admin Panel by <span className='text-secondary'>Saksham Verma</span></p>
+                    <div className="buttons d-flex gap-1">
+                    <Button variant='secondary'><Link to='/login' className='text-decoration-none text-white'>Login</Link></Button>
+                    <Button variant='secondary'><Link to='/signup' className='text-decoration-none text-white'>Signup</Link></Button>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
+export default Home;
