f185df374d194ffd4cf5d7d8667ca740742a9422
Saksham Verma <sakshamverma626@gmail.com>
2024-07-05 01:31:22 +0530

    i've tried every single thing to deploy the backend to the vercel the backend is deploying correctly but the signup login request are not working basically anykind of request to server is not working. i am done for today now i am going to sleep

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://admin-panel-server-seven.vercel.app";
+export const host = "http://localhost:5000";
 export const login = `${host}/api/auth/login`;
 export const signup = `${host}/api/auth/signup`;
 export const getUsers = `${host}/api/auth/get`;
