ef2241deb1ace9f52e6b28d1af2d2f11c243fc58
Saksham Verma <sakshamverma626@gmail.com>
2024-07-05 01:33:45 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,27 +8,29 @@ const { getUsers } = require("./controllers/userController");
 const app = express();
 require("dotenv").config();
 
-const allowedOrigins = ['http://localhost:5173', "https://admin-panel-public.vercel.app/"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://admin-panel-public.vercel.app/",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, origin);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true, // Enable sending of cookies and other credentials
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 // Enable preflight for all routes
-app.options('*', cors(corsOptions));
-
+app.options("*", cors(corsOptions));
 
 app.use("/api/auth", userRoutes);
 app.use("/get", getUsers);
@@ -42,6 +44,7 @@ const server = app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on ${process.env.PORT}`);
 });
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
