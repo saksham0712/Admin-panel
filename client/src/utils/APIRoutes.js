04f31dec2ee91ba7bee874473b337ddf2ea57036
Saksham Verma <sakshamverma626@gmail.com>
2024-07-04 19:00:18 +0530

    update vercel.json

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://admin-panel-one-sooty.vercel.app";
 export const login = `${host}/api/auth/login`;
 export const signup = `${host}/api/auth/signup`;
 export const getUsers = `${host}/api/auth/get`;
