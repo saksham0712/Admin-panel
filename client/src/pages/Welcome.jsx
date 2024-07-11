c52fa7c9898e3d12d7287ae35ab3cfafe6a6d930
Saksham Verma <sakshamverma626@gmail.com>
2024-07-12 01:17:57 +0530

    Update Welcome.jsx

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -11,7 +11,6 @@ const Home = () => {
                     <h2>
                         Welcome
                     </h2>
-<h4>Abhinav Tiwari</h4>
                     <p>Admin Panel by <span className='text-secondary'>Saksham Verma</span></p>
                     <div className="buttons d-flex gap-1">
                         <Button variant='secondary'><Link to='/login' className='text-decoration-none text-white'>Login</Link></Button>
